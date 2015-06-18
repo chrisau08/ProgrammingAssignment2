af77cd3448e6017c1fed2abbe6d340a40de060c8
Christopher Kenny <christopher.kenny1@gmail.com>
2015-06-18 08:14:26 -0400

    Added comment with example invertible matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Example of invertible matrix, matrix(c(2,2,3,2),nrow=2,ncol=2)
+## When solve(x) applied the result is matrix(c(-1,1,1.5,-1.0),nrow=2,ncol=2)
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  x
 }
 
 
