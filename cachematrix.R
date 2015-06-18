17e4916291e6fbf79a6c4786961e2c279420432d
Chris Kenny <christopher.kenny1@gmail.com>
2015-06-18 19:48:39 -0400

    Added comments to Example

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,9 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Sample invertible matrix, matrix(c(2,2,3,2),nrow=2,ncol=2)
+## Evaluates to matrix(c(-1,1,1.5,-1.0),nrow=2,ncol=2) using solve(x)
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
