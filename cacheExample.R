3b66894335bfc50fc86ba59bd5cbab00836e1cd9
Christopher Kenny <christopher.kenny1@gmail.com>
2015-06-18 13:24:16 -0400

    Caching Example from Instructions
    Only created so that I could add commenting and better understand the
    flow of the code.

---
+++ cacheExample.R
@@ -0,0 +1,31 @@
+## Example of caching using vectors and mean (credit: Robert Peng)
+## From the instructions in Programming Assignment 2 of R Programming.
+
+## This file exists so I can comment and notate what each part is doing.
+## This is to better understand the concepts and flow.
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
